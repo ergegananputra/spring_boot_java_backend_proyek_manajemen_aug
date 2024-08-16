d290aa60214540b62030fc2a25a0045eb4b677ae
ergegananputra <126530940+ergegananputra@users.noreply.github.com>
2024-08-16 10:39:17 +0700

    feat: ResponseJSON

--- src/main/java/com/adielboanerge/seiproject/controller/LokasiController.java
+++ src/main/java/com/adielboanerge/seiproject/controller/LokasiController.java
@@ -1,6 +1,7 @@
 package com.adielboanerge.seiproject.controller;
 
 import com.adielboanerge.seiproject.dto.RequestBodyID;
+import com.adielboanerge.seiproject.dto.ResponseJSON;
 import com.adielboanerge.seiproject.entity.Lokasi;
 import com.adielboanerge.seiproject.service.LokasiService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,22 +17,73 @@ public class LokasiController {
     private LokasiService lokasiService;
 
     @PostMapping("")
-    public Lokasi postLokasi(@RequestBody Lokasi lokasi) {
-        return lokasiService.createLokasi(lokasi);
+    public ResponseJSON<Lokasi> postLokasi(@RequestBody Lokasi lokasi) {
+        ResponseJSON<Lokasi> responseJSON = new ResponseJSON<>();
+
+        try {
+            Lokasi lokasiCreated = lokasiService.createLokasi(lokasi);
+            responseJSON.setMessage("Data Lokasi Berhasil Dibuat");
+            responseJSON.setData(lokasiCreated);
+            return responseJSON;
+        } catch (Exception e) {
+            responseJSON.setMessage("Terjadi Kesalahan");
+            responseJSON.setData(null);
+            return responseJSON;
+        }
     }
 
     @GetMapping("")
-    public List<Lokasi> getLokasis() {
-        return lokasiService.getLokasis();
+    public ResponseJSON<List<Lokasi>> getLokasis() {
+        ResponseJSON<List<Lokasi>> responseJSON = new ResponseJSON<>();
+
+        try {
+            List<Lokasi> lokasis = lokasiService.getLokasis();
+            if (lokasis.isEmpty()) {
+                responseJSON.setMessage("Data Lokasi Kosong");
+                responseJSON.setData(null);
+            } else {
+                responseJSON.setMessage("Data Lokasi Ditemukan");
+                responseJSON.setData(lokasis);
+            }
+            return responseJSON;
+        } catch (Exception e) {
+            responseJSON.setMessage("Terjadi Kesalahan");
+            responseJSON.setData(null);
+            return responseJSON;
+        }
+
+
     }
 
     @PutMapping("")
-    public Lokasi putLokasi(@RequestBody Lokasi lokasi) {
-        return lokasiService.update(lokasi);
+    public ResponseJSON<Lokasi> putLokasi(@RequestBody Lokasi lokasi) {
+        ResponseJSON<Lokasi> responseJSON = new ResponseJSON<>();
+
+        try {
+            Lokasi lokasiUpdated = lokasiService.update(lokasi);
+            responseJSON.setMessage("Data Lokasi Berhasil Diupdate");
+            responseJSON.setData(lokasiUpdated);
+            return responseJSON;
+        } catch (Exception e) {
+            responseJSON.setMessage("Terjadi Kesalahan");
+            responseJSON.setData(null);
+            return responseJSON;
+        }
     }
 
     @DeleteMapping("")
-    public Lokasi deleteLokasi(@RequestBody RequestBodyID requestBodyID) {
-        return lokasiService.delete(requestBodyID);
+    public ResponseJSON<Lokasi> deleteLokasi(@RequestBody RequestBodyID requestBodyID) {
+        ResponseJSON<Lokasi> responseJSON = new ResponseJSON<>();
+
+        try {
+            Lokasi lokasiDeleted = lokasiService.delete(requestBodyID);
+            responseJSON.setMessage("Data Lokasi Berhasil Dihapus");
+            responseJSON.setData(lokasiDeleted);
+            return responseJSON;
+        } catch (Exception e) {
+            responseJSON.setMessage("Terjadi Kesalahan");
+            responseJSON.setData(null);
+            return responseJSON;
+        }
     }
 }
--- src/main/java/com/adielboanerge/seiproject/controller/ProyekController.java
+++ src/main/java/com/adielboanerge/seiproject/controller/ProyekController.java
@@ -2,6 +2,7 @@ package com.adielboanerge.seiproject.controller;
 
 import com.adielboanerge.seiproject.dto.RequestBodyID;
 import com.adielboanerge.seiproject.dto.RequestProyekLokasi;
+import com.adielboanerge.seiproject.dto.ResponseJSON;
 import com.adielboanerge.seiproject.entity.Proyek;
 import com.adielboanerge.seiproject.service.ProyekService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,17 +23,55 @@ public class ProyekController {
     }
 
     @GetMapping("")
-    public List<Proyek> getProyeks() {
-        return proyekService.getProyeks();
+    public ResponseJSON<List<Proyek>> getProyeks() {
+        ResponseJSON<List<Proyek>> responseJSON = new ResponseJSON<>();
+
+        try {
+            List<Proyek> proyeks = proyekService.getProyeks();
+            if (proyeks.isEmpty()) {
+                responseJSON.setMessage("Data Proyek Kosong");
+                responseJSON.setData(null);
+            } else {
+                responseJSON.setMessage("Data Proyek Ditemukan");
+                responseJSON.setData(proyeks);
+            }
+            return responseJSON;
+        } catch (Exception e) {
+            responseJSON.setMessage("Terjadi Kesalahan");
+            responseJSON.setData(null);
+            return responseJSON;
+        }
     }
 
     @PutMapping("")
-    public Proyek putProyek(@RequestBody Proyek proyek) {
-        return proyekService.update(proyek);
+    public ResponseJSON<Proyek> putProyek(@RequestBody Proyek proyek) {
+        ResponseJSON<Proyek> responseJSON = new ResponseJSON<>();
+
+        try {
+            Proyek proyekUpdated = proyekService.update(proyek);
+            responseJSON.setMessage("Data Proyek Berhasil Diupdate");
+            responseJSON.setData(proyekUpdated);
+            return responseJSON;
+        } catch (Exception e) {
+            responseJSON.setMessage("Terjadi Kesalahan");
+            responseJSON.setData(null);
+            return responseJSON;
+        }
     }
 
     @DeleteMapping("")
-    public Proyek deleteProyek(@RequestBody RequestBodyID requestBodyID) {
-        return  proyekService.deleteProyek(requestBodyID);
+    public ResponseJSON<Proyek> deleteProyek(@RequestBody RequestBodyID requestBodyID) {
+        ResponseJSON<Proyek> responseJSON = new ResponseJSON<>();
+
+        try {
+            Proyek proyekDeleted = proyekService.deleteProyek(requestBodyID);
+            responseJSON.setMessage("Data Proyek Berhasil Dihapus");
+            responseJSON.setData(proyekDeleted);
+            return responseJSON;
+        } catch (Exception e) {
+            responseJSON.setMessage("Terjadi Kesalahan");
+            responseJSON.setData(null);
+            return responseJSON;
+        }
     }
 }
